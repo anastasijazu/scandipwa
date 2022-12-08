18b192e3d151393152518d89ac7e6717335b414b
Anastasija Zubkova <anastasijazubkova@Anastasijas-MacBook-Air.local>
2022-12-08 12:32:01 +0200

    UI_Typography WIP

---
+++ packages/scandipwa/src/ui/Typography/Typography.component.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @scandipwa/scandipwa-guidelines/jsx-no-props-destruction */
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/scandipwa
+ * @link https://github.com/scandipwa/scandipwa
+ */
+
+import { HTMLAttributes, PureComponent } from 'react';
+
+import { ReactElement } from 'Type/Common.type';
+
+import { TypographyVariants } from './Typography.config';
+import { TypographyComponentProps, TypographyMapVariant } from './Typography.type';
+
+import './Typography.style';
+
+/** @namespace ui/Typography/Component */
+export class TypographyComponent extends PureComponent<TypographyComponentProps> {
+    variantMap: Record<TypographyVariants, TypographyMapVariant> = {
+        [TypographyVariants.H1]: {
+            Component: 'h1',
+            block: 'h1',
+        },
+        [TypographyVariants.H2]: {
+            Component: 'h2',
+            block: 'h2',
+        },
+        [TypographyVariants.H3]: {
+            Component: 'h3',
+            block: 'h3',
+        },
+        [TypographyVariants.H4]: {
+            Component: 'h4',
+            block: 'h4',
+        },
+        [TypographyVariants.P]: {
+            Component: 'p',
+            block: 'p',
+        },
+        [TypographyVariants.CAPTION]: {
+            Component: 'p',
+            block: 'caption',
+        },
+    };
+
+    renderVariant(): ReactElement {
+        const {
+            variant, children, mix, attr, appearance,
+        } = this.props;
+        const { Component, block } = this.variantMap[variant];
+
+        if (Component) {
+            return (
+                <Component
+                  block={ appearance || block }
+                  mix={ mix }
+                  { ...attr as HTMLAttributes<HTMLElement> }
+                >
+                  { children }
+                </Component>
+            );
+        }
+
+        return null;
+    }
+
+    render(): ReactElement {
+        return this.renderVariant();
+    }
+}
+
+export default TypographyComponent;
---
+++ packages/scandipwa/src/ui/Typography/Typography.config.ts
@@ -0,0 +1,19 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/scandipwa
+ * @link https://github.com/scandipwa/scandipwa
+ */
+
+export enum TypographyVariants {
+    H1 = 'h1',
+    H2 = 'h2',
+    H3 = 'h3',
+    H4 = 'h4',
+    P = 'p',
+    CAPTION = 'caption',
+}
